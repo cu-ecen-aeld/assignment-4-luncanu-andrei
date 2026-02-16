f2f39a1d25434c2271be88dc020323100a884db7
luncanu <luncanu@Ubuntu.myguest.virtualbox.org>
2026-02-16 14:12:06 +0200

    Use local site method for aesd-assignments to bypass SSH errors

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,31 +1,20 @@
-##############################################################
-#
-# AESD-ASSIGNMENTS
-#
-##############################################################
-
-# Use 'HEAD' to always pull the latest, or a specific commit hash
 AESD_ASSIGNMENTS_VERSION = 'HEAD'
-# Ensure this is your Assignment 3 SSH URL
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-luncanu-andrei.git'
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+# This points to the folder containing your assignment code (writer, finder, etc.)
+# relative to the buildroot directory
+AESD_ASSIGNMENTS_SITE = $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/aesd-assignments
+AESD_ASSIGNMENTS_SITE_METHOD = local
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
+# Ensure the files are installed to the target /usr/bin
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(TARGET_DIR)/usr/bin
-	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf
-	
-	# Install binaries and scripts to /usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
-	
-	# Install config files to /etc/finder-app/conf/
-	$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/conf/assignment.txt $(TARGET_DIR)/usr/bin/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/username.txt $(TARGET_DIR)/usr/bin/conf/
 endef
 
 $(eval $(generic-package))
